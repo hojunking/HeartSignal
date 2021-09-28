fd103a6f8ec28d4316d8d9043ca583429d0ecf25
username <wpdlxldpf4@gmail.com>
2021-09-28 17:57:29 +0900

    2021-09-28 loginSuccessHandler pth

--- meetme/src/main/java/com/hs/meetme/config/SecurityConfig.java
+++ meetme/src/main/java/com/hs/meetme/config/SecurityConfig.java
@@ -11,11 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -30,34 +30,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
 //  security를 적용하지 않을 url 경로
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/slick/**", "/font-awesome-4.7.0/**");
+        web.ignoring()
+        	.antMatchers("/css/**", "/js/**", "/img/**", "/slick/**", "/font-awesome-4.7.0/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/member/**").authenticated()
-                .antMatchers("/admin/**").authenticated()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/s/member").access("hasRole('USER')")
+                .antMatchers("/s/admin").access("hasRole('ADMIN')")
+                .antMatchers("/s/all").permitAll();
 
-        http.formLogin();
-//                .loginPage("/login")
-//                .defaultSuccessUrl("/")
-//                .permitAll();
+        http.formLogin()
+                .loginPage("/s/login")
+                .defaultSuccessUrl("/")
+                .permitAll();
 
         http.logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/s/logout"))
+                .logoutSuccessUrl("/s/login")
                 .invalidateHttpSession(true);
 
         http.exceptionHandling()
-                .accessDeniedPage("/denied");
+                .accessDeniedPage("/s/denied");
+        
+        http.csrf().disable();
     }
-
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
-//    }
-
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.inMemoryAuthentication()
+    			.withUser("admin")
+    			.password("{noop}admin")
+    			.roles("ADMIN");
+    }
+    
+    public AuthenticationSuccessHandler loginSuccessHandler() {
+    	return new LoginSuccess();
+    }
 }
