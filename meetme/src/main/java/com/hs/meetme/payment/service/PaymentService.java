6688b3bdb2ffda501e06c6e871141408194e6d68
hojunking <gkwlal3@naver.com>
2021-09-29 19:01:15 +0900

    ignore rv

--- meetme/src/main/java/com/hs/meetme/payment/service/PaymentService.java
+++
@@ -1,9 +0,0 @@
-package com.hs.meetme.payment.service;
-
-import com.hs.meetme.payment.domain.PaymentVO;
-
-public interface PaymentService {
-	public int paymentInsert(PaymentVO vo);
-	public String test(PaymentVO vo);
-
-}
--- meetme/src/main/java/com/hs/meetme/sample/domain/TagVO.java
+++
@@ -1,8 +0,0 @@
-package com.hs.meetme.sample.domain;
-
-import lombok.Data;
-
-@Data
-public class TagVO {
-	private String tagId;
-}
