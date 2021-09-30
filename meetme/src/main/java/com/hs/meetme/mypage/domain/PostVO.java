141b20638390ba7f3c9a64afaa6266bca067b3e5
username <wpdlxldpf4@gmail.com>
2021-09-30 09:04:04 +0900

    2021-09-29 complete

---
+++ meetme/src/main/java/com/hs/meetme/mypage/domain/PostVO.java
@@ -0,0 +1,29 @@
+package com.hs.meetme.mypage.domain;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class PostVO {
+  
+	private String postId; //게시물 번호 시퀀스
+	private String postTitle; 
+	private String postContent;
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+	private Date postCreated;
+	private Date postUpdated;
+	private String userId; //작성자 id
+	private String courseId;//코스 id
+	private String hit;
+	private String postDistinct;//커뮤니티 c, 추천 r
+	
+	  
+	//다른 테이블 꺼
+	private String up; //게시글 추천수
+  
+	private String postCount;      //게시글 갯수
+  
+}
