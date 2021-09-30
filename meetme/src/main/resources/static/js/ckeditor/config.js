4719e7be697deafb5d9302d066f75bf72ce71820
inyoeng <79848622+inyoeng@users.noreply.github.com>
2021-09-30 19:04:31 +0900

    09-30 inyeong

--- meetme/src/main/resources/static/js/ckeditor/config.js
+++ meetme/src/main/resources/static/js/ckeditor/config.js
@@ -35,4 +35,6 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+	//readonly
+	// config.readOnly = true;CKEDITOR.editor.setReadOnly;
 };
