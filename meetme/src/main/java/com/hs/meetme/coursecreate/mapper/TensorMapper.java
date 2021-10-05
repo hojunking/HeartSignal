e47c48a094d169d424ebc2c5177e130071ad303a
Han Jae Ho <chacha_95@naver.com>
2021-10-05 16:03:27 +0900

    jaeHoCommit

---
+++ meetme/src/main/java/com/hs/meetme/coursecreate/mapper/TensorMapper.java
@@ -0,0 +1,21 @@
+package com.hs.meetme.coursecreate.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.hs.meetme.coursecreate.domain.TensorCauseVO;
+import com.hs.meetme.coursecreate.domain.TensorResultVO;
+import com.hs.meetme.coursecreate.domain.TensorVO;
+
+@Mapper
+public interface TensorMapper {
+	
+	// tensor를 이용하기 위한 데이터를 가져오는 것
+	List<TensorVO> createTensorData();
+	
+	List<TensorCauseVO> createTensorCauseData();
+	
+	List<TensorResultVO> createTensorResultData();
+	
+}
