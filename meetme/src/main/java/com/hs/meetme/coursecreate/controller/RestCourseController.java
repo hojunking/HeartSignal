9ed8e57fc4e6c1632311bdc799707404d81d7b57
username <wpdlxldpf4@gmail.com>
2021-10-07 13:54:47 +0900

    course create transactional add

--- meetme/src/main/java/com/hs/meetme/coursecreate/controller/RestCourseController.java
+++ meetme/src/main/java/com/hs/meetme/coursecreate/controller/RestCourseController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -104,6 +105,7 @@ public class RestCourseController {
 	
 	// 코스 등록
 	@PostMapping("/register")
+	@Transactional
 	public String registerCourse(String courseName, String list, HttpServletRequest request) throws JsonProcessingException {
 		System.out.println(list);
 		HttpSession session = request.getSession();
