893d329566e4b1f1762f97d0d3e3a8008461c793
A.A.W <wpdlxldpf4@gmail.com>
2021-10-08 09:34:48 +0900

    criteria error solved

--- meetme/src/main/java/com/hs/meetme/post/mapper/PostMapper.java
+++ meetme/src/main/java/com/hs/meetme/post/mapper/PostMapper.java
@@ -3,11 +3,9 @@ package com.hs.meetme.post.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import com.hs.meetme.mypage.domain.CommentVO;
 import com.hs.meetme.mypage.domain.Criteria;
-import com.hs.meetme.mypage.domain.Criteria2;
 import com.hs.meetme.mypage.domain.MyPageCourseVO;
 import com.hs.meetme.mypage.domain.PostVO;
 
