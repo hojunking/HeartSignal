4e8ebbb15f217200280fadd68c0977c968f3e39a
phoebe <cheezepeperoni@gmail.com>
2021-10-11 18:10:01 +0900

    BMJ couplelog 2021/10/11

---
+++ meetme/src/main/java/com/hs/meetme/coupleLog/controller/RestCoupleLogController.java
@@ -0,0 +1,69 @@
+package com.hs.meetme.coupleLog.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.hs.meetme.coupleLog.domain.CoupleLogVO;
+import com.hs.meetme.coupleLog.service.CoupleLogService;
+
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+@RequestMapping("/coupleLog/*")
+public class RestCoupleLogController {
+	@Autowired CoupleLogService service;
+	
+	File fileDir = new File("src/main/resources/static/img/");
+	
+	
+	@PostMapping("/coupleLogImg")
+	public CoupleLogVO updateCoupleImage(MultipartFile file, CoupleLogVO vo)
+			throws IllegalStateException, IOException {
+		String path = fileDir.getAbsolutePath() + "/coupleLog/";
+		System.out.println(path);
+		MultipartFile ufile = file;
+		if (!ufile.isEmpty() && ufile.getSize() > 0) {
+			String filename = ufile.getOriginalFilename();
+			UUID uuid = UUID.randomUUID();
+			String imgUrl = uuid + filename;
+			File uufile = new File(path, imgUrl);
+			System.out.println(uufile.getPath());
+			ufile.transferTo(uufile); //파일 옮기기
+			vo.setImgUrl(imgUrl);
+			System.out.println("이미지URL=" + vo.getImgUrl());
+			service.logInsertImg(vo);
+			System.out.println(vo.toString());
+			
+		}
+
+		return vo;
+	}
+	/*
+	 * @GetMapping("/readImage") public ResponseEntity<byte[]>
+	 * readImage(@PathVariable("imagename") String imagename) throws IOException {
+	 * String path = fileDir.getAbsolutePath() + "/coupleLog/"; InputStream
+	 * imageStream = new FileInputStream(path + imagename); byte[] imageByteArray =
+	 * IOUtils.toByteArray(imageStream); imageStream.close(); return new
+	 * ResponseEntity<byte[]>(imageByteArray, HttpStatus.OK); }
+	 */
+	
+}
--- meetme/src/main/java/com/hs/meetme/coupleLog/mapper/CoupleLogMapper.java
+++ meetme/src/main/java/com/hs/meetme/coupleLog/mapper/CoupleLogMapper.java
@@ -8,7 +8,7 @@ import com.hs.meetme.coupleLog.domain.CoupleLogVO;
 
 @Mapper
 public interface CoupleLogMapper {
-	public List <CoupleLogVO> getLogList();//로그 전체 불러오기
+	public List <CoupleLogVO> getLogList(String coupleId);//로그 전체 불러오기
 	public CoupleLogVO getLog(CoupleLogVO vo);//로그 하나 가져오기
 	
 	public int logInsert(CoupleLogVO vo); //로그 insert
