732936a67ce60496ad4b8995a4cb92e0e7503991
phoebe <cheezepeperoni@gmail.com>
2021-10-19 18:47:34 +0900

    BMJ COUPLECOURSE

--- meetme/src/main/java/com/hs/meetme/coupleCourse/controller/CoupleCourseRestController.java
+++ meetme/src/main/java/com/hs/meetme/coupleCourse/controller/CoupleCourseRestController.java
@@ -1,8 +1,5 @@
 package com.hs.meetme.coupleCourse.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hs.meetme.coupleCourse.domain.CoupleCourseVO;
 import com.hs.meetme.coupleCourse.service.CoupleCourseService;
-import com.hs.meetme.useraccess.domain.AccountVO;
 
 @RestController
 @RequestMapping("/coupleCourse/*")
@@ -22,18 +18,12 @@ public class CoupleCourseRestController {
 
 	@DeleteMapping("/deleteCourse")
 	@Transactional
-	public boolean deleteCourse(@RequestBody CoupleCourseVO vo, HttpServletRequest request) {
+	public boolean deleteCourse(@RequestBody CoupleCourseVO vo) {
 
-		// 세션 쓰는법
-		HttpSession session = request.getSession();
-		AccountVO accountVO = (AccountVO) session.getAttribute("userSession");
-
+		System.out.println(vo);
+		
 		courseService.deleteCourseOrder(vo);
 		int r = courseService.deleteCourse(vo);
-		if (r == 1) {
-			return true;
-		} else {
-			return false;
-		}
+		return true;
 	}
 }
